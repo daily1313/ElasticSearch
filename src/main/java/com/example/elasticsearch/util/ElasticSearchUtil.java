0614688d4a12ed10eaa0631d97b15378874d74a1
김승범 <kimsb7218@naver.com>
2023-11-08 21:43:02 +0900

    feat: boolQuery를 적용하기 위한 Util 클래스 추가

--- src/main/java/com/example/elasticsearch/util/ElasticSearchUtil.java
+++ src/main/java/com/example/elasticsearch/util/ElasticSearchUtil.java
@@ -1,7 +1,12 @@
 package com.example.elasticsearch.util;
 
+import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.MatchAllQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.MatchQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch._types.query_dsl.TermQuery;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.val;
 
 import java.util.function.Supplier;
@@ -16,5 +21,30 @@ public class ElasticSearchUtil {
     public static MatchAllQuery matchAllQuery() {
         val matchAllQuery = new MatchAllQuery.Builder();
         return matchAllQuery.build();
+    }
+
+    public static Supplier<Query> supplierQueryForBoolQuery(final String productName, final Integer qty) {
+        Supplier<Query> supplier = () -> Query.of(q->q.bool(boolQuery(productName, qty)));
+        return supplier;
+    }
+
+    public static BoolQuery boolQuery(final String productName, final Integer qty) {
+        val boolQuery = new BoolQuery.Builder();
+        return boolQuery.filter(termQuery(productName)).must(matchQuery(qty)).build();
+        // 필터 안에 가장 많은 쿼리 반환
+    }
+
+    public static List<Query> termQuery(final String productName) {
+        final List<Query> terms = new ArrayList<>();
+        val termQuery = new TermQuery.Builder();
+        terms.add(Query.of(q->q.term(termQuery.field("name").value(productName).build())));
+        return terms;
+    }
+
+    public static List<Query> matchQuery(final Integer qty) {
+        final List<Query> matches = new ArrayList<>();
+        val matchQuery = new MatchQuery.Builder();
+        matches.add(Query.of(q->q.match(matchQuery.field("name").query(qty).build())));
+        return matches;
     }
 }
